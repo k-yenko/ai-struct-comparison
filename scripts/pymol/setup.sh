cc898c40adb3ecfcf38a717e0a2661f03798d4c4
k-yenko <katherineayenko@gmail.com>
2024-12-10 17:38:15 -0800

    add pymol bash setup file

---
+++ scripts/pymol/setup.sh
@@ -0,0 +1,9 @@
+# Step 1: Install necessary build tools (if not already installed)
+# For macOS, you may need to install Xcode command line tools
+xcode-select --install
+
+# Step 2: Install PyMOL using Homebrew
+brew install pymol
+
+# Step 3: Run PyMOL
+pymol
